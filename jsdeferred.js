35d995508c7807790ac04aa9b5584b7e04360805
cho45 <cho45@lowreal.net>
2012-05-02 11:01:41 +0900

    fix comment

--- jsdeferred.js
+++ jsdeferred.js
@@ -26,10 +26,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- */
-; // no warnings for uglify
+ */ ; // no warnings for uglify
+
 /*
- * doc comment
+ * Syntax of comments:
  * http://code.google.com/intl/ja/closure/compiler/docs/js-for-compiler.html
  */
 
